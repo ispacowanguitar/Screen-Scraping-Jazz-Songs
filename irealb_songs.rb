c212cc382f63265dc193abb4b3a55e461e492334
ACLTC <acltc@ACLTCs-MacBook-Air.local>
2016-01-24 22:06:22 -0600

    fixed indentation issues

--- irealb_songs.rb
+++ irealb_songs.rb
@@ -20,45 +20,46 @@ composer.delete_at(634)
 index = 0
 1300.times do
 
-  url[index].sub!("irealb://[url=irealb://", "")
-  url[index].sub!("irealb://", "")
+    url[index].sub!("irealb://[url=irealb://", "")
+    url[index].sub!("irealb://", "")
 
-  14.times do
-  url[index].sub!("%3D", ", ")
-  url[index].sub!("%20", ", ")
-  end
-  
-  url[index] = url[index].split(", ")
-  url[index].pop
-  style = ""
-  if url[index].include?("Swing")
-    style = "Swing"
-  elsif url[index].include?("Bossa")
-    style = "Bossa"
-  elsif url[index].include?("Waltz")
-    style = "Waltz"
-  elsif url[index].include?("Ballad")
-    style = "Ballad"
-  elsif url[index].include?("Even" && "8ths")
-    style = "Even 8ths"
-  elsif url[index].include?("Latin")
-    style = "Latin"
-  elsif url[index].include?("Funk")
-    style = "Funk"
-  elsif url[index].include?("Afro")
-    style = "Afro"
-  elsif url[index].include?("Samba")
-    style = "Samba"
-  elsif url[index].include?("Rock")
-    style = "Rock"
-  else
-    style = "unknown"
-  end
-
-      file << "{title: \"#{title[index]}\"" + ", style:" + " \"#{style}\""+ ", composer: " "\"#{composer[index]}\"" + "},\n"
-
-  index += 1
+    14.times do
+      url[index].sub!("%3D", ", ")
+      url[index].sub!("%20", ", ")
     end
+    
+    url[index] = url[index].split(", ")
+    url[index].pop
+    style = ""
+    if url[index].include?("Swing")
+      style = "Swing"
+    elsif url[index].include?("Bossa")
+      style = "Bossa"
+    elsif url[index].include?("Waltz")
+      style = "Waltz"
+    elsif url[index].include?("Ballad")
+      style = "Ballad"
+    elsif url[index].include?("Even" && "8ths")
+      style = "Even 8ths"
+    elsif url[index].include?("Latin")
+      style = "Latin"
+    elsif url[index].include?("Funk")
+      style = "Funk"
+    elsif url[index].include?("Afro")
+      style = "Afro"
+    elsif url[index].include?("Samba")
+      style = "Samba"
+    elsif url[index].include?("Rock")
+      style = "Rock"
+    else
+      style = "unknown"
+    end
+
+    file << "{title: \"#{title[index]}\"" + ", style:" + " \"#{style}\""+ ", composer: " "\"#{composer[index]}\"" + "},\n"
+
+    index += 1
+    
+  end
 end
 
 
